f28ec2ab3e484a820cd327cfb6b0a6dd81f57d83
Roelant Cornelissen <roelantcornelissen@gmail.com>
2024-10-31 15:07:04 +0100

    Minor refactoring

--- ConfluenceSpaceExportReplaceTool/Program.cs
+++ ConfluenceSpaceExportReplaceTool/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.IO.Compression;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using ConfluenceSpaceExportReplaceTool.Configuration;
 using ConfluenceSpaceExportReplaceTool.Logic;
 
@@ -10,27 +9,28 @@ internal static partial class Program
     internal static void Main()
     {
         Console.WriteLine(@"Example input: '.\Confluence-export-space-[OldSpaceKey].zip'");
-        Console.WriteLine("Provide path where to find the confluence space export:");
-        var zipPath = Console.ReadLine();
+        Console.WriteLine("Provide the path where to find the confluence space export:");
+        var zipFilePath = Console.ReadLine();
         
-        if (string.IsNullOrEmpty(zipPath))
+        if (string.IsNullOrEmpty(zipFilePath))
         {
             Console.WriteLine("Invalid path to the .zip file provided");
             return;
         }
         
-        if (!zipPath.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
+        if (!zipFilePath.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
         {
             Console.WriteLine("Invalid zip file provided");
             return;
         }
 
-        if (!File.Exists(zipPath))
+        if (!File.Exists(zipFilePath))
         {
             Console.WriteLine("Zip file provided does not exist");
             return;
         }
 
+        Console.WriteLine("The space key should be composed of only alphanumeric characters (a-z, A-Z, 0-9) and should be no more than 255 characters in length");
         Console.WriteLine("Provide old space key:");
         var oldSpaceKey = Console.ReadLine()?.ToUpperInvariant();
 
@@ -42,30 +42,32 @@ internal static partial class Program
 
         if (!SpaceKeyRegex().IsMatch(oldSpaceKey))
         {
-            Console.WriteLine("Old space key is invalid \n\n Composed of only alphanumeric characters (a-z, A-Z, 0-9) \n\n No more than 255 characters in length");
+            Console.WriteLine("Old space key is invalid and should be composed of only alphanumeric characters (a-z, A-Z, 0-9) and should be no more than 255 characters in length");
             return;
         }
 
+        Console.WriteLine("The space key should be composed of only alphanumeric characters (a-z, A-Z, 0-9) and should be no more than 255 characters in length");
         Console.WriteLine("Provide new space key:");
-        var newSpacekey = Console.ReadLine()?.ToUpperInvariant();
+        var newSpaceKey = Console.ReadLine()?.ToUpperInvariant();
 
-        if (string.IsNullOrEmpty(newSpacekey))
+        if (string.IsNullOrEmpty(newSpaceKey))
         {
             Console.WriteLine("New space key cannot be null or empty");
             return;
         }
 
-        if (!SpaceKeyRegex().IsMatch(newSpacekey))
+        if (!SpaceKeyRegex().IsMatch(newSpaceKey))
         {
-            Console.WriteLine("New space key is invalid \n\n Composed of only alphanumeric characters (a-z, A-Z, 0-9) \n\n No more than 255 characters in length");
+            Console.WriteLine("New space key is invalid and should be composed of only alphanumeric characters (a-z, A-Z, 0-9) and should be no more than 255 characters in length");
             return;
         }
         
         var zipExtractionDirectory = Path.GetFullPath(Constants.ZipExtractionDirectory);
-        var confluenceSpaceExportZip = Path.GetFullPath(zipPath);
-        var confluenceSpaceImportZip = ConfluenceSpaceImportGenerator.GenerateConfluenceSpaceImportZip(confluenceSpaceExportZip, zipExtractionDirectory, oldSpaceKey, newSpacekey);
+        var confluenceSpaceExportZip = Path.GetFullPath(zipFilePath);
+        var confluenceSpaceImportZip = ConfluenceSpaceImportGenerator.GenerateConfluenceSpaceImportZip(confluenceSpaceExportZip, zipExtractionDirectory, oldSpaceKey, newSpaceKey);
 
         Console.WriteLine($"Your Confluence space import zip file can be found here: {Path.GetFullPath(confluenceSpaceImportZip)}");
+        Console.ReadLine();
     }
 
     [GeneratedRegex(RegexPatterns.SpaceKeyRegexPattern)]
